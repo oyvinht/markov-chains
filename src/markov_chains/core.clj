7549528a1abf5e2431cb35df6ee251d89d98bd16
Øyvin Halfdan Thuv <oyvin.h.thuv@uit.no>
2021-04-29 22:44:52 +0200

    Adding code & basic tests.

--- src/markov_chains/core.clj
+++ src/markov_chains/core.clj
@@ -1,6 +1,292 @@
 (ns markov-chains.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+;;;;---------------------------------------------------------------------------
+;;;; General helper code.
+;;;;---------------------------------------------------------------------------
+(defn sum [fn seq]
+  "Map fn over seq and sum all results."
+  (apply + (map fn seq)))
+
+;;;;---------------------------------------------------------------------------
+;;;; Code for generating matrices from example data.
+;;;;---------------------------------------------------------------------------
+(defn- transition-counts [state-sequences]
+  "Create nested hash map with transition counts: i->j->count."
+  (reduce
+   (fn [hash-map state-sequence]
+     (loop [states state-sequence
+            i nil ; Previous state
+            j (first states) ; Current state
+            transition-counts hash-map]
+       (if (not j)
+         transition-counts
+         (recur
+          (rest states)
+          j
+          (first (rest states))
+          (assoc-in transition-counts [i j]
+                    (inc (or (get-in transition-counts [i j]) 0)))))))
+   {}
+   state-sequences))
+
+(defn- outcome-counts [state-outcomes]
+  "Create a nested hash map with with outcome M from state N: N->M->count."
+  (reduce (fn [counts occurence]
+            (assoc-in counts occurence (inc (or (get-in counts occurence) 0))))
+          {}
+          state-outcomes))
+
+(defn- counts->probs [count-hash-map]
+  "Convert from count hash-map to probability hash-map."
+  (into {} (for [[k cnts] count-hash-map
+                 :let [cnts-sum (apply + (vals cnts))]]
+             {k (into {} (for [[v cnt] cnts]
+                           {v (/ cnt cnts-sum)}))})))
+
+(defn- transition-probs [state-sequences]
+  "Create a nested hash map with transition probabilities i->j->prob."
+  (counts->probs (transition-counts state-sequences)))
+
+(defn- outcome-probs [state-outcomes]
+  "Create a nested hash map with outcome probabilities k->v->prob."
+  (counts->probs (outcome-counts state-outcomes)))
+
+;;;;---------------------------------------------------------------------------
+;;;; Code for creating HMMs.
+;;;;---------------------------------------------------------------------------
+(defstruct hmm
+  :A ; Per state probability of moving to each next state
+  :B ; Per state probability of observing any of M
+  :π ; Per state probability of being the sequence starting point
+  )
+
+(defn make-hmm [state-sequences state-outcomes]
+  "Create a HMM from a seq of state sequences and a seq of state outcomes."
+  (let [tp (transition-probs state-sequences)
+        op (outcome-probs state-outcomes)]
+    (struct-map hmm :A (dissoc tp nil) :B op :π (get tp nil))))
+
+(defn init-hmm [init-probs transition-probs outcome-probs]
+  (struct-map hmm :A transition-probs :B outcome-probs :π init-probs))
+
+;;;;---------------------------------------------------------------------------
+;;;; Code for working on HMMs
+;;;;---------------------------------------------------------------------------
+(defn states [hmm]
+  "Return a list of states (state names) for an HMM."
+  (keys (:A hmm)))
+
+(defn transition-prob [hmm from-state to-state]
+  "Get probability of transitioning directly from from-state to to-state."
+  (get-in (:A hmm) [from-state to-state] 0))
+
+(defn outcome-prob [hmm state outcome]
+  "Get probability of observing outcome from the given state."
+  (get-in (:B hmm) [state outcome] 0))
+
+(defn init-prob [hmm state]
+  "Get probability of being in state at start time."
+  (get (:π hmm) state 0))
+
+(defn init-probs [hmm]
+  "Get probabilities of alle states at start time."
+  (:π hmm))
+       
+(defn- n [m]
+  "Helper: Normalize map of probabilities."
+  (let [s (apply + (vals  m))]
+    (if (== s 0)
+      m
+      (into {} (for [[k v] m] {k (/ v s)})))))
+
+(defn- m [seq-of-pairs]
+  "Helper: Convert seq of 2-elem vectors into a map."
+  (into {} seq-of-pairs))
+
+(defn forward [hmm observations]
+  "Build an ordered list of maps with forward probabilities (alphas) per state."
+  (loop [α [(m (map (fn [s] ; Base case
+                      {s (* (init-prob hmm s)
+                            (outcome-prob hmm s (first observations)))})
+                    (states hmm)))]
+         obs (rest observations)]
+    (if (empty? obs)
+      α
+      (recur (concat α
+                     [(m
+                       (for [s_t+1 (states hmm)]
+                         {s_t+1 (apply ; Inductive case
+                                 + (map
+                                    (fn [α_s]
+                                      (* (val α_s)
+                                         (transition-prob hmm (key α_s) s_t+1)
+                                         (outcome-prob hmm s_t+1 (first obs))))
+                                    (nth α (dec (count α)))))}))])
+             (rest obs)))))
+
+(defn sequence-prob [hmm observations]
+  "The probability of seing a the sequence of observations given hmm: P(O|λ)."
+  (let [fwds (forward hmm observations)]
+    (sum (fn [s] (get (last fwds) s)) (states hmm))))
+
+(defn backward [hmm observations]
+  "Build an ordered list of maps with backward probabilities (betas) per state."
+  (loop [β (list (m (map (fn [s] {s 1}) (states hmm))))
+         obs observations]
+    (if (empty? obs)
+      β
+      (recur (concat
+              [(m (for [s (states hmm)]
+                    {s (apply + (map (fn [β_s_t+1]
+                                       (* (val β_s_t+1)
+                                          (transition-prob hmm s (key β_s_t+1))
+                                          (outcome-prob hmm
+                                                        (key β_s_t+1)
+                                                        (last obs))))
+                                     (first β)))}))]
+              β)
+             (butlast obs)))))
+
+(defn forward-backward [hmm observations]
+  (let [fwds (forward hmm observations)
+        bwds (backward hmm observations)]
+    (map (fn [fw bw] (m (for [[k v] fw] {k (* v (get bw k))})))
+         (cons (init-probs hmm) fwds)
+         bwds)))
+
+(defn- argmax [paths]
+  (loop [paths paths best (first paths)]
+    (if (empty? paths)
+      best
+      (recur (rest paths)
+             (if (> (:prob (first paths)) (:prob best)) (first paths) best)))))
+
+(defn viterbi [hmm observations]
+  "Find sequence of states with highest likelihood of explaining observations."
+  (loop [obs (rest observations)
+         paths (for [s (states hmm)]
+                 {:path (list s)
+                  :prob (* (init-prob hmm s)
+                           (outcome-prob hmm s (first observations)))})]
+    (if (empty? obs)
+      (argmax paths)
+      (recur
+       (rest obs)
+       (map (fn [state]
+              (let [best (argmax
+                          (map (fn [path]
+                                 {:path (concat (:path path) (list state))
+                                  :prob (* (:prob path)
+                                           (transition-prob
+                                            hmm (last (:path path)) state))})
+                               paths))]
+                {:path (:path best)
+                 :prob (* (:prob best) (outcome-prob hmm state (first obs)))}))
+            (states hmm))))))
+
+;;;;---------------------------------------------------------------------------
+;;;; Code for learning HMM from observations.
+;;;;---------------------------------------------------------------------------
+(defn rand-transition-probs [states]
+  "Create a nested hash map with random transition probs between states."
+  (into {}
+        (map (fn [i]
+               {i (into {} (map (fn [j] {j (rand)}) states))})
+             states)))
+
+(defn rand-outcome-probs [states vocabulary]
+  "Create a nested hash map with random outcome probs from states."
+  (into {}
+        (map (fn [i]
+               {i (into {} (map (fn [j] {j (rand)}) vocabulary))})
+             states)))
+
+(defn random-hmm [num-hidden-states]
+  (let [init-states (take num-hidden-states (repeatedly (fn [] (gensym "S-"))))
+        init (into {} (for [s init-states] {s (/ 1 num-hidden-states)}))
+        A (rand-transition-probs init-states)
+        B (rand-outcome-probs init-states '[N E])]
+    (init-hmm init A B)))
+
+(defn baum-welch [hmm observations num-iterations]
+  "Return an improved HMM by training on observations num-iteration times."
+  (let [fwds (forward hmm observations)
+        bwds (rest (backward hmm observations))]
+    (letfn
+        [(ξ [t i j]
+           ;; Expected state transition count:
+           ;; Probability of being in state 'i' at time 't'
+           ;; then in state 'j' at time 't+1'.
+           (/ (* (get (nth fwds t) i)
+                 (transition-prob hmm i j)
+                 (outcome-prob hmm j (nth observations (inc t)))
+                 (get (nth bwds (inc t)) j))
+              (sum (fn [i]
+                     (sum (fn [j]
+                            (* (get (nth fwds t) i)
+                               (transition-prob hmm i j)
+                               (outcome-prob hmm j (nth observations (inc t)))
+                               (get (nth bwds (inc t)) j)))
+                          (states hmm)))
+                   (states hmm))))
+         (γ [t i]
+           ;; Expected state occupancy count.
+           (/ (* (get (nth fwds t) i)
+                 (get (nth bwds t) i))
+              (sum (fn [s]
+                     (* (get (nth fwds t) s)
+                        (get (nth bwds t) s)))
+                   (states hmm))))
+         (a [i j]
+           ;; Excpected num transitions divided by total num transitions from i.
+           (/ (sum (fn [t] (ξ t i j)) (range (dec (count observations))))
+              (sum (fn [t] (γ t i)) (range (dec (count observations))))))
+         (b [j v_k]
+           ;; Expected num times in j observing v_k divided by
+           ;; by tot expected num of times in j.
+           (/ (sum (fn [t] (if (= v_k (nth observations t)) (γ t j) 0))
+                   (range (count observations)))
+              (sum (fn [t] (γ t j)) (range (count observations)))))]
+      (let [new-init (into {} (map (fn [s] {s (γ 0 s)}) (states hmm)))
+            new-A (into {} (map (fn [i]
+                                  {i (n (m (map (fn [j]
+                                                  {j (a i j)})
+                                                (states hmm))))})
+                                (states hmm)))
+            new-B  (into {} (map (fn [j] {j (n (m (map (fn [o]
+                                                         {o (b j o)})
+                                                       (set observations))))})
+                                 (states hmm)))
+            new-hmm (init-hmm new-init new-A new-B)]
+        (comment
+          (doall
+           (map (fn [x]
+                  (println
+                   (γ x (nth (states hmm) x))
+                   "="
+                   (sum (fn [j] (ξ x (nth (states hmm) x) j)) (states hmm)) "?"
+                   (= (sum (fn [j] (ξ x (nth (states hmm) x) j)) (states hmm))
+                      (γ x (nth (states hmm) x)))))
+                (range (count (states hmm))))))
+        ;; (println "Forwards:")
+        ;; (println fwds)
+        ;; (println "New PI:" new-init)
+        ;; (println "New A:")
+        ;; ;(clojure.pprint/pprint new-A)
+        ;; (println "New B:")
+        ;; ;(clojure.pprint/pprint new-B)
+        ;; (println "FWDS" (apply + (vals (last fwds))))
+        (if (= num-iterations 0)
+          new-hmm
+          (baum-welch new-hmm observations (dec num-iterations)))
+        ))))
+  
+(defn test-baum-welch []
+  (let [hmm (init-hmm
+             {:s1 0.2 :s2 0.8}
+             {:s1 {:s1 0.5 :s2 0.5}
+              :s2 {:s1 0.3 :s2 0.7}}
+             {:s1 {:N 0.3 :E 0.7}
+              :s2 {:N 0.8 :E 0.2}})]
+
+    (baum-welch hmm [:N :N :N :N :N :E :E :N :N :N] 1)))
--- test/markov_chains/core_test.clj
+++ test/markov_chains/core_test.clj
@@ -1,7 +1,116 @@
 (ns markov-chains.core-test
   (:require [clojure.test :refer :all]
-            [markov-chains.core :refer :all]))
+            [markov-chains.core :as sut]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;;;;---------------------------------------------------------------------------
+;;;; Example state sequences and state outcomes.
+;;;;---------------------------------------------------------------------------
+(def example-state-sequences-1
+  '[[a a a a b c a b d a d b f]
+    [a a f a b b c d a a c]
+    [a a b]
+    [c a c f]
+    [a a f b d e a]
+    [f c c a b e d a a f a c]])
+
+(def example-state-outcomes-1
+  '[[a 0][a 0][a 0][c 0][c 2][b 0][b 0][e 0][b 0][a 1][b 2][a 1][c 4][d 1][a 1]
+    [b 0][d 6][e 0][a 15][c 4][c 2][a 0][b 8][a 2][e 5][a 2][d 4][c 20][e 9]])
+
+(deftest make-hmm-test
+  (testing "Creating HMM from example data."
+    (def hmm (sut/make-hmm example-state-sequences-1
+                           example-state-outcomes-1))
+    (is (not (nil? hmm)))))
+
+(deftest transition-prob
+  (testing "Checking probability of transitioning from state a to b (25%)."
+    (let [hmm (sut/make-hmm example-state-sequences-1
+                            example-state-outcomes-1)]
+      (is (= (sut/transition-prob hmm 'a 'b) 1/4)))))
+
+(deftest outcome-prob
+  (testing "Checking probability of getting outcome 2 from state b (1/6)."
+    (let [hmm (sut/make-hmm example-state-sequences-1
+                            example-state-outcomes-1)]
+      (is (= (sut/outcome-prob hmm 'b 2) 1/6)))))
+
+;;;;---------------------------------------------------------------------------
+;;;; Umbrella example from Russel & Norvig.
+;;;;---------------------------------------------------------------------------
+(def example-hmm-rn
+  (sut/init-hmm {:rain 0.5 :no-rain 0.5}
+                {:rain {:rain 0.7 :no-rain 0.3}
+                 :no-rain {:rain 0.3 :no-rain 0.7}}
+                {:rain {:umbrella 0.9 :no-umbrella 0.1}
+                 :no-rain {:umbrella 0.2 :no-umbrella 0.8}}))
+                
+
+(deftest test-hmm-rn-fw
+  (testing "Checking alphas of forward algorithm from Russel & Norvig."
+    (is
+     (= (list
+         {:rain 0.81818181818181810, :no-rain 0.18181818181818182}
+         {:rain 0.88335704125177800, :no-rain 0.11664295874822190}
+         {:rain 0.19066793972352525, :no-rain 0.80933206027647480}
+         {:rain 0.73079400458498200, :no-rain 0.26920599541501794}
+         {:rain 0.86733888957548470, :no-rain 0.13266111042451528})
+        (sut/forward example-hmm-rn
+                     [:umbrella :umbrella :no-umbrella :umbrella :umbrella])))))
+
+(deftest test-hmm-rn-bw
+  (testing "Testing backward algorithm on Russel & Norvig example."
+    (is
+     (= (list
+         {:rain 0.6469355558301939, :no-rain 0.3530644441698061}
+         {:rain 0.5923176018339928, :no-rain 0.4076823981660072}
+         {:rain 0.37626717588941005, :no-rain 0.6237328241105898}
+         {:rain 0.6533428165007112, :no-rain 0.34665718349928876}
+         {:rain 0.6272727272727272, :no-rain 0.37272727272727274}
+         {:rain 1, :no-rain 1})
+        (sut/backward
+         example-hmm-rn
+         [:umbrella :umbrella :no-umbrella :umbrella :umbrella])))))
+
+(deftest test-hmm-rn-fw-bw
+  (testing "Testing forward-backward on Russel & Norvig example."
+    (is
+     (= (list
+         {:rain 0.64693555583019390 :no-rain 0.35306444416980610}
+         {:rain 0.86733888957548470 :no-rain 0.13266111042451528}
+         {:rain 0.82041905362367540 :no-rain 0.17958094637632463}
+         {:rain 0.30748357600661774 :no-rain 0.69251642399338230}
+         {:rain 0.82041905362367530 :no-rain 0.17958094637632466}
+         {:rain 0.86733888957548470 :no-rain 0.13266111042451528})
+        (sut/forward-backward
+         example-hmm-rn
+         [:umbrella :umbrella :no-umbrella :umbrella :umbrella])))))
+
+;;;;---------------------------------------------------------------------------
+;;;; Example from Wikipedia.
+;;;;---------------------------------------------------------------------------
+(deftest test-hmm-wp
+  (testing "Testing forward-backward from Wikipedia example."
+    (let [h (sut/init-hmm {"Healthy" 0.6 "Fever" 0.4}
+                          {"Healthy" {"Healthy" 0.69 "Fever" 0.3 "E" 0.01}
+                           "Fever" {"Healthy" 0.4 "Fever" 0.59 "E" 0.01}}
+                          {"Healthy" {"normal" 0.5 "cold" 0.4 "dizzy" 0.1}
+                           "Fever" {"normal" 0.1 "cold" 0.3 "dizzy" 0.6}})]
+                          
+      (is (= (list
+              {"Healthy" 0.68308190362154340 "Fever" 0.31691809637845664}
+              {"Healthy" 0.87701103755732590 "Fever" 0.12298896244267407}
+              {"Healthy" 0.62322803095095390 "Fever" 0.37677196904904610}
+              {"Healthy" 0.21095270484130565 "Fever" 0.78904729515869430})
+             (sut/forward-backward h ["normal" "cold" "dizzy"]))))))
+
+(deftest test-hmm-viterbi-wp
+  (testing "Testing Viterbi algorithm on Wikipedia example."
+    (let [h (sut/init-hmm {"Healthy" 0.6 "Fever" 0.4}
+                          {"Healthy" {"Healthy" 0.7 "Fever" 0.3}
+                           "Fever" {"Healthy" 0.4 "Fever" 0.6}}
+                          {"Healthy" {"normal" 0.5 "cold" 0.4 "dizzy" 0.1}
+                           "Fever" {"normal" 0.1 "cold" 0.3 "dizzy" 0.6}})]
+                          
+      (is (= {:path '("Healthy" "Healthy" "Fever"), :prob 0.01512}
+             (sut/viterbi h ["normal" "cold" "dizzy"]))))))
