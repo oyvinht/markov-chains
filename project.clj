63840affe7ae927b0b15226a8b8044fec60ab5ab
Øyvin Halfdan Thuv <oyvin.h.thuv@uit.no>
2021-06-21 15:03:32 +0200

    Fix project.clj descriptions

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject net.clojars.oyvinht/markov-chains "0.9.3"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "A library for working with Markov Chains."
+  :url "https://github.com/oyvinht/markov-chains"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[com.clojure-goes-fast/clj-async-profiler "0.5.0"]
