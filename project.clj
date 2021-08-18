9a308fcbf897862340988bc140e2979e3aa5958d
Øyvin Halfdan Thuv <oyvin.h.thuv@uit.no>
2021-08-18 14:16:26 +0200

    Update licence

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject net.clojars.oyvinht/markov-chains "0.9.3"
   :description "A library for working with Markov Chains."
   :url "https://github.com/oyvinht/markov-chains"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :license {:name "MIT"
+            :url "LICENCE.TXT"}
   :dependencies [[com.clojure-goes-fast/clj-async-profiler "0.5.0"]
                  [criterium "0.4.6"]
                  [org.clojure/clojure "1.10.1"]]
